09ab43fed75ce4423e00d175f4c0bce841382876
zgq <coiggahou>
2020-11-29 17:06:32 +0800

    Initial commit

---
+++ TestGit/src/domain/Student.java
@@ -0,0 +1,10 @@
+package domain;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private Integer id;
+    private String name;
+    private String password;
+    
+}
